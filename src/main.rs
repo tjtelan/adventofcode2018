caae375ed0196921c3c45ec16a8f351b632dbc22
T.J. Telan <t.telan@gmail.com>
2018-12-03 13:54:02 -0800

    Cleaning up the commented code, but leaving the other hacks

--- src/main.rs
+++ src/main.rs
@@ -11,54 +11,39 @@ fn main() -> Result<()> {
     let display = path.display();
     let mut file = File::open(&path)?;
 
-    let mut resulting_frequency = 0; // Part one answer
+    let mut resulting_frequency = 0;
     let mut frequency_history : HashSet<i32>= HashSet::new();
     let mut frequency_dupes : Vec<i32> = Vec::new();
     let mut count = 0;
 
+    // Read in the file, and convert into a Vec<i32> so we can
     let mut file_contents = String::new();
     file.read_to_string(&mut file_contents)
         .ok()
         .expect("failed to read!");
+    drop(file);
     let period : Vec<i32> = file_contents.split("\n")
         .filter_map(|s: &str| s.to_string().parse::<i32>().ok())
         .collect();
 
+    // Looping once like this, so we can get the answer to part 1
     for f_delta in &period {
-
         count += 1;
-        //println!("Back at it again for the {:?} time", count);
-
-        // Being cool with unwrap() bc this really should crash if the file has bad input
-        //let frequency_change = line.parse::<i32>().unwrap();
-
-
-        //println!("Found: {:?}", &frequency_change);
         resulting_frequency += f_delta;
-        //println!("Running total: {:?}", &resulting_frequency);
 
         if !frequency_history.insert(resulting_frequency.clone()) {
             frequency_dupes.push(resulting_frequency.clone());
             break;
         }
     }
-
     println!("Resulting frequency, for part1: {:?}", &resulting_frequency);
-
 
     // Hack
     while frequency_dupes.len() == 0 {
-
         count += 1;
-        //println!("Back at it again for the {:?} time", count);
 
         for f_delta in &period {
-            // Being cool with unwrap() bc this really should crash if the file has bad input
-            //let frequency_change = line.unwrap().parse::<i32>().unwrap();
-
-            //println!("Found: {:?}", &frequency_change);
             resulting_frequency += f_delta;
-            //println!("Running total: {:?}", &resulting_frequency);
     
             if !frequency_history.insert(resulting_frequency.clone()) {
                 frequency_dupes.push(resulting_frequency.clone());
